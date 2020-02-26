f4c92e16c0a4492dc961a6885b85797816b494bc
Ershovas146 <Ershovas146@gmail.com>
2020-02-26 17:43:32 +0300

    add canvas

--- src/components/ThirdSlide/Content/Content.js
+++ src/components/ThirdSlide/Content/Content.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Content.module.css';
 
 const Content= () => {
+
+    const drawSome = () => {
+      let canvas = document.getElementById('canvas')
+      let ctx = canvas.getContext("2d");
+      canvas.width = canvas.parentElement.offsetWidth;
+      canvas.height = canvas.parentElement.offsetHeight;
+      ctx.font = 'bold 5em Georgia'
+      ctx.fillStyle = 'white'
+      ctx.fillText(1, canvas.width/2, 50)
+    };
+    useEffect(() => {
+      drawSome();
+    });
+    window.addEventListener("resize", function() {
+      drawSome();
+    });
     return (
-      <div>
+      <div className={classes.content}>
+        <canvas id="canvas" ></canvas>
       </div>
     );
-  }
+}
   
   export default Content;
